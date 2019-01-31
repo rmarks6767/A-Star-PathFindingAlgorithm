f7d0add3dd286900905a64178b3cc5fc1165efca
River Marks <rmarks6767@gmail.com>
2019-01-30 21:00:20 -0500

    Added the remainder of the code to the project

--- PathFinding/Program.cs
+++ PathFinding/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace PathFinding
 {
@@ -10,7 +11,25 @@ namespace PathFinding
     {
         static void Main(string[] args)
         {
+            Grid grid = new Grid(100, 100);
 
+            Path path = new Path();
+
+            List<Node> totalPath = path.GeneratePath(new Vector2(0, 0), new Vector2(0, 40), grid);
+
+            for (int i = 0; i < totalPath.Count; i++)
+            {
+                grid.grid[totalPath[i].X, totalPath[i].Y] = 'O';
+            }
+
+            for (int i = 0; i < 100; i++)
+            {
+                for (int ii = 0; ii < 100; ii++)
+                {
+                    Console.Write(grid.grid[i, ii]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
